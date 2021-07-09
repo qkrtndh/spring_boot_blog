31d76527350022ffcbfc3647425679bcefcd39d5
qkrtndh <qkrtndh5@naver.com>
2021-07-09 19:42:42 +0900

    responsedto 수정

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -22,6 +22,6 @@ public class UserApiController {
 		System.out.println("무야호");
 		user.setRole(RoleType.USER);
 		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK,result);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),result);
 	}
 }
