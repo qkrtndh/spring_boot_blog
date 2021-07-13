0dda536c8993d6746f6cba142ae4a43975357ae2
qkrtndh <qkrtndh5@naver.com>
2021-07-13 16:58:03 +0900

    글목록

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,16 +3,22 @@ package com.cos.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
-@Controller
+@Controller//일반 Controller는 리턴시 viewResolver가 동작함
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 
 	@GetMapping({"","/"})
-	public String index() {
-		return "index";
+	public String index(Model model) {
+		model.addAttribute("boards",boardService.글목록());
+		return "index";//viewResolver가 동작하면 model정보를 가지고 인덱스페이지로 이동 반환값 앞뒤로 prefix suffix 붙여줌
 	}
 	
 	@GetMapping("/board/saveForm")
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -2,6 +2,8 @@ package com.cos.blog.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,11 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+		
+	}
+	
+	public List<Board> 글목록(){
+		return boardRepository.findAll();
 	}
 	
 	/*@Transactional(readOnly = true) //select 시 트랜젝션 시작, 서비스 종료시 트랜잭션 종료(정합성 유지)
