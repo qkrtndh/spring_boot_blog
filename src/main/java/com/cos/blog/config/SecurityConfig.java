ec2cd0494004e15bf0b30b37cc4da8766b7fbafe
qkrtndh <qkrtndh5@naver.com>
2021-07-14 17:06:16 +0900

    회원정보 수정(2/2)

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cos.blog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,13 @@ import com.cos.blog.config.auth.PrincipalDetailService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)//특정주소로 접근하면 권한 및 인증을 미리 체크한다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Autowired
 	private PrincipalDetailService principalDetailService;
 	
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,12 +4,19 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
@@ -17,33 +24,40 @@ import com.cos.blog.service.UserService;
 
 @RestController
 public class UserApiController {
-	
+
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
 	@PostMapping("/auth/joinProc")
-	public  ResponseDto<Integer> save(@RequestBody User user) {
+	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("회원가입");
 		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(),result);		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);
 	}
+
 	@PutMapping("/user")
-	public  ResponseDto<Integer> update(@RequestBody User user) {
+	public ResponseDto<Integer> update(@RequestBody User user) {
 		System.out.println("회원수정");
 		userService.회원수정(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);		
+		// 트랜잭션이 종료되어 DB값은변경되었으나 세션값은 변경되지 않은 상태
+		// 세션값을 직접 변경해야함.
+		// 세션 등록
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-	//스프링 시큐리티가 화면을 가로채서 로그인을 유도한다. 이때 비밀번호는 콘솔에 뜨며 아이디는 user.
-	//세션은 자동으로 생성된다.
-	
+	// 스프링 시큐리티가 화면을 가로채서 로그인을 유도한다. 이때 비밀번호는 콘솔에 뜨며 아이디는 user.
+	// 세션은 자동으로 생성된다.
+
 	/*
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user,HttpSession session){
-		System.out.println("login 호출");
-		User principal = userService.로그인(user);
-		if(principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
-	}*/
+	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
+	 * login(@RequestBody User user,HttpSession session){
+	 * System.out.println("login 호출"); User principal = userService.로그인(user);
+	 * if(principal != null) { session.setAttribute("principal", principal); }
+	 * return new ResponseDto<Integer>(HttpStatus.OK.value(),1); }
+	 */
 }
