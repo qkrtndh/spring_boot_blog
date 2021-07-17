09e99806906d8dc63fdfde7c9dac3d42e6528878
qkrtndh <qkrtndh5@naver.com>
2021-07-17 18:05:36 +0900

    댓글 목록 표시,무한참조 방지

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -18,6 +18,8 @@ import javax.persistence.OneToMany;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -53,7 +55,9 @@ public class Board {
 	
 	@OneToMany(mappedBy = "board",fetch = FetchType.EAGER)//mappedby는 연관관계의 주인이 아니므로 .fk 가 아니며, db에 칼럼을 생성하지 않도록한다.
 	//이렇게 함으로서 board 테이블을 불러올 때 reply를 join만 하게 할 수 있다.
-	private List<Reply> reply;
+	@JsonIgnoreProperties({"board"})
+	//내부의 board의 getter를 하지 않는다.(무한참조방지)
+	private List<Reply> replys;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
