e5602b09a96da0a994bd7336150163bb7004dd70
qkrtndh <qkrtndh5@naver.com>
2021-07-20 12:50:55 +0900

    댓글 작성시 네이티브 쿼리 사용

--- src/main/java/com/cos/blog/repository/ReplyRepository.java
+++ src/main/java/com/cos/blog/repository/ReplyRepository.java
@@ -1,9 +1,17 @@
 package com.cos.blog.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.cos.blog.dto.ReplySaveRequestDto;
 import com.cos.blog.model.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer>{
-
+	
+	@Modifying
+	@Query(value="INSERT into reply(userId, boardId, content,createDate) values(?1,?2,?3,now())",nativeQuery = true)
+	int mSave(int userId, int boardId, String content);
 }
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -72,21 +72,8 @@ public class BoardService {
 	
 	@Transactional
 	public void 댓글쓰기(ReplySaveRequestDto replySaveRequestDto) {
-		User user = userRepository.findById(replySaveRequestDto.getUserId()).orElseThrow(()->{
-			return new IllegalArgumentException("댓글 쓰기 실패 : 유저 ID를 찾을 수 없습니다.");
-		});//영속화 완료
-		
-		Board board = boardRepository.findById(replySaveRequestDto.getBoardId()).orElseThrow(()->{
-			return new IllegalArgumentException("댓글 쓰기 실패 : 게시글 ID를 찾을 수 없습니다.");
-		});//영속화 완료
-		
-		Reply reply = Reply.builder()
-				.user(user)
-				.board(board)
-				.content(replySaveRequestDto.getContent())
-				.build();
-		
-		replyRepository.save(reply);
+				
+		replyRepository.mSave(replySaveRequestDto.getUserId(),replySaveRequestDto.getBoardId(),replySaveRequestDto.getContent());
 	}
 	
 	
